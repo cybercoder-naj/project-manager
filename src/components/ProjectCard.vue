e9397a9700902f44a064425a84ff9a9bfe0c4db7
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-07-09 20:24:07 +0530

    Added project dummy data

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card flat class="pa-3">
+    <v-row :class="`pas-3 project ${project.status}`">
+      <v-col cols="12" md="6">
+        <div class="caption grey--text">Project Title</div>
+        <div>{{ project.title }}</div>
+      </v-col>
+      <v-col cols="6" sm="4" md="2">
+        <div class="caption grey--text">Person</div>
+        <div>{{ project.person }}</div>
+      </v-col>
+      <v-col cols="6" sm="4" md="2">
+        <div class="caption grey--text">Due by</div>
+        <div>{{ project.due }}</div>
+      </v-col>
+      <v-col cols="12" sm="4" md="2">
+        <div class="caption grey--text">Status</div>
+        <div>{{ project.status }}</div>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props: ['project'],
+};
+</script>
+
+<style>
+.project.complete {
+    border-left: 4px solid #3cd1c2;
+}
+
+.project.ongoing {
+    border-left: 4px solid orange;
+}
+
+.project.overdue {
+    border-left: 4px solid tomato;
+}
+
+.project::after {
+    content: '';
+    width: 100%;
+    background-color: lightgrey;
+    height: 1px;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,32 +3,54 @@
     <h1 class="mx-4 subtitle-1 grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-      <v-card flat class="pa-3">
-        <v-row>
-          <v-col cols="12" md="6">
-            <div class="caption grey--text">Project Title</div>
-            <div>Create a new Website</div>
-          </v-col>
-          <v-col cols="6" sm="4" md="2">
-            <div class="caption grey--text">Person</div>
-            <div>Nishant</div>
-          </v-col>
-          <v-col cols="6" sm="4" md="2">
-            <div class="caption grey--text">Due by</div>
-            <div>10th July 2021</div>
-          </v-col>
-          <v-col cols="12" sm="4" md="2">
-            <div class="caption grey--text">Status</div>
-            <div>Ongoing</div>
-          </v-col>
-        </v-row>
-      </v-card>
+      <ProjectCard v-for="project in projects" :key="project.title" :project="project" />
     </v-container>
   </div>
 </template>
 
 <script>
+import ProjectCard from '@/components/ProjectCard.vue'
+
 export default {
-  name: "Home",
+  name: "Dashboard",
+  components: { ProjectCard },
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+      ],
+    };
+  },
 };
 </script>
