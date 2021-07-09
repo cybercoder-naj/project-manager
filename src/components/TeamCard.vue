fd2075c0ea0292dae9ff5323f6b2b5ea17f03a28
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-07-09 21:03:58 +0530

    Added code for team page

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-col cols="12" sm="6" md="4" lg="3">
+    <v-card class="text-center ma-3">
+      <v-responsive class="pt-4"> image goes here </v-responsive>
+      <v-card-text>
+        <div class="text-subtitle-1">{{ person.name }}</div>
+        <div class="grey--text">{{ person.role }}</div>
+      </v-card-text>
+      <v-card-action>
+        <v-btn depressed class="grey--text">
+          <v-icon small left>mdi-message</v-icon>
+          <span>Message</span>
+        </v-btn>
+      </v-card-action>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "TeamCard",
+  props: ["person"],
+};
+</script>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,13 +3,29 @@
     <h1 class="mx-4 subtitle-1 grey--text">Team</h1>
 
     <v-container class="my-5">
-        content
+      <v-row>
+        <TeamCard v-for="person in team" :key="person.name" :person="person" />
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
+import TeamCard from "@/components/TeamCard.vue";
+
 export default {
   name: "Team",
+  components: { TeamCard },
+  data() {
+    return {
+      team: [
+        { name: "The Net Ninja", role: "Web developer" },
+        { name: "Ryu", role: "Graphic designer" },
+        { name: "Chun Li", role: "Web developer" },
+        { name: "Gouken", role: "Social media maverick" },
+        { name: "Yoshi", role: "Sales guru" },
+      ],
+    };
+  },
 };
 </script>
