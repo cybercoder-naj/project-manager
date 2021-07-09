c21f3eff958852a87d1352885b6115f6e0a77fb4
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-07-09 16:54:39 +0530

    Added margin and padding

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="dasboard">
-    <h1>Dashboard</h1>
+    <h1 class="mx-4 subtitle-1 grey--text">Dashboard</h1>
+
+    <v-container class="my-5">
+        content
+    </v-container>
   </div>
 </template>
 
