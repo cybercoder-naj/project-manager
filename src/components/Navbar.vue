16b73f74e54f057cd1d01532745141a3ed2c72ef
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-07-09 16:03:34 +0530

    Toolbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+      <v-app-bar flat>
+          <v-toolbar-title class="text-uppercase grey--text">
+              <span class="font-weight-light">Project</span>
+              <span>Manager</span>
+          </v-toolbar-title>
+          <v-spacer />
+          <v-btn text color="grey">
+              <span>Sign Out</span>
+              <v-icon right>mdi-exit-to-app</v-icon>
+          </v-btn>
+      </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
